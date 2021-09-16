e8caf1ef7a308bbf9ee74dfcf7e16f8f12c1d96b
alisha0217 <69092959+alisha0217@users.noreply.github.com>
2021-09-15 21:50:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 song1 = ""
 song2 = ""
 
+scoreLeftWrist=0;
+scoreRightWrist=0;
+
+song1_status = ""
+song2_status = ""
+
 leftWristX = 0;
 leftWristY = 0;
 
@@ -33,6 +39,9 @@ function gotPoses(results){
 
         console.log(results);
 
+        scoreLeftWrist = results[0].pose.keypoints[9].score
+        scoreRightWrist = results[0].pose.keypoints[10].score
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
 
@@ -43,10 +52,52 @@ function gotPoses(results){
 
         console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY );
 
+
+
     }
 }
 
-function draw(){
-    image(video, 0, 0, 600, 500);
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+			document.getElementById("song").innerHTML = " Playing - Upbeat Music"
+		}
+	}
+	if(scoreLeftWrist > 0.2)
+	{ 
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song1_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = " Playing - Disco Music"
+		}
+	}
+	
 }
+
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
+
 
